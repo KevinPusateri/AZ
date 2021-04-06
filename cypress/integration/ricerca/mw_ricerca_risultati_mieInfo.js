b960ff51d7332880d8431c9b6f2725aa0a652184
Kevin Pusateri <andrea.oboe@allianz.it>
2021-04-06 09:10:11 +0200

    Update mw_ricerca_risultati_mieInfo.js

--- cypress/integration/ricerca/mw_ricerca_risultati_mieInfo.js
+++ cypress/integration/ricerca/mw_ricerca_risultati_mieInfo.js
@@ -25,9 +25,101 @@ afterEach(() => {
 })
 
 describe('Buca di Ricerca - Risultati Le mie Info', function () {
-    it('Verifica Ricerca una parola ',function(){
+    
+    //TODO modifica controllo elenco + verifica suggerimeti
+    it('Verifica Ricerca Incasso',function(){
         cy.get('input[name="main-search-input"]').click()
         cy.get('input[name="main-search-input"]').type('incasso').type('{enter}').wait(2000)
+        cy.url().should('include','search/infos/circulars')
+
+        const tabs = ['clients', 'sales', 'le mie info'];
+        cy.get('[class="docs-grid-colored-row tabs-container nx-grid__row"]').contains('le mie info').should('have.class','active')
+        cy.get('[class="docs-grid-colored-row tabs-container nx-grid__row"]').find('a').should('have.length',3)
+           .each(($tab, i) => {
+                expect($tab.text()).to.include(tabs[i]);
+           });
+    
+        const tabsContainer = ['Circolari', 'Company Handbook'];
+        cy.get('[class="lib-tab-info nx-grid"]').find('[href^="/matrix/search/infos"]').should('have.length',2).each(($tab, i) =>{
+            expect($tab.text()).to.include(tabsContainer[i]);
+        })
+        
+        cy.get('[class="lib-tab-info nx-grid"]').contains('Circolari')
+        cy.get('lib-circular-item').each($circular =>{
+            cy.wrap($circular).find('[class="date"]').then($date =>{
+                if($date.text().trim().length > 0){
+                    cy.wrap($date).should('contain',$date.text().trim()) 
+                }else{
+                    assert.fail('Manca data su un elemento della pagina circulars')
+                }
+            }) 
+            cy.wrap($circular).find('[class="network"]').then($company =>{
+                if($company.text().trim().length > 0){
+                    cy.wrap($company).should('contain',$company.text().trim()) 
+                }else{
+                    assert.fail('Manca compagnia su un elemento della pagina circulars')
+                }
+            }) 
+            cy.wrap($circular).find('[class="info"]').then($info =>{
+                if($info.text().trim().length > 0){
+                    cy.wrap($info).should('contain',$info.text().trim()) 
+                }else{
+                    assert.fail('Manca info a chi sono indirizzate su un elemento della pagina circulars')
+                }
+            }) 
+            cy.wrap($circular).find('[class="title"]').then($title =>{
+                if($title.text().trim().length > 0){
+                    cy.wrap($title).should('contain',$title.text().trim()) 
+                }else{
+                    assert.fail('Manca titolo su un elemento della pagina circulars')
+                }
+            }) 
+
+        })
+
+         cy.get('[class="lib-tab-info nx-grid"]').contains('Company Handbook').click()
+
+         for(var i = 0; i <10; i++){
+            cy.get('#lib-handbook-container').scrollTo('bottom').wait(1000)
+         }
+         cy.get('lib-handbooks-item').then($hanbooks =>{
+
+            cy.wrap($hanbooks).find('[class="date"]').each($date =>{
+                if($date.text().trim().length > 0){
+                    cy.wrap($date).should('contain',$date.text().trim())
+                }else{
+                    assert.fail('Manca data su un elemento della pagina handbook')
+                }
+            }) 
+            cy.wrap($hanbooks).find('[class="lib-badge handbook"]').contains('handbook').each($badge =>{
+                if($badge.text().trim().length > 0){
+                    cy.wrap($badge).should('contain',$badge.text().trim())
+                }else{
+                    assert.fail('Manca badge su un elemento della handbook')
+                }
+            })
+            cy.wrap($hanbooks).find('[class="title"]').each($title =>{
+                if($title.text().trim().length > 0){
+                    cy.wrap($title).should('contain', $title.text().trim())
+                }else{
+                    assert.fail('Manca titolo su un elemento della su pagina handbook')
+                }
+            }) 
+            cy.wrap($hanbooks).find('[class="text"]').each($text =>{
+                if($text.text().substring(0,5).trim().length > 0){
+                    cy.wrap($text).should('contain', $text.text().trim().substring(0,5))
+                }else{
+                    assert.fail('Manca un\'anteprima del testo su un elemento della pagina handbook')
+                }
+            }) 
+
+         })
+    })
+    
+    //TODO modifica controllo elenco + verifica suggerimeti
+    it('Verifica Ricerca Fastquote',function(){
+        cy.get('input[name="main-search-input"]').click()
+        cy.get('input[name="main-search-input"]').type('fastquote').type('{enter}').wait(2000)
         cy.url().should('include','search/infos/circulars')
 
         const tabs = ['clients', 'sales', 'le mie info'];
@@ -112,6 +204,59 @@ describe('Buca di Ricerca - Risultati Le mie Info', function () {
             }) 
 
          })
+    })
+
+
+    //TODO modifica controllo elenco + verifica suggerimeti
+    it.only('Verifica Ricerca Proddotto: Ultra',function(){
+        cy.get('input[name="main-search-input"]').click()
+        cy.get('input[name="main-search-input"]').type('ultra').type('{enter}').wait(2000)
+        cy.url().should('include','matrix/search/clients/clients')
+
+        const tabs = ['clients', 'sales', 'le mie info'];
+        cy.get('[class="docs-grid-colored-row tabs-container nx-grid__row"]').contains('clients').should('have.class','active')
+        cy.get('[class="docs-grid-colored-row tabs-container nx-grid__row"]').find('a').should('have.length',3)
+           .each(($tab, i) => {
+                expect($tab.text()).to.include(tabs[i]);
+           });
+    
+        const tabsContainer = ['Clienti'];
+        cy.get('[class="lib-tab-info nx-grid"]').find('[href^="/matrix/search/clients/clients"]').should('have.length',1).each(($tab, i) =>{
+            expect($tab.text()).to.include(tabsContainer[i]);
+        })
+        
+        cy.get('[class="lib-tab-info nx-grid"]').contains('Clienti')
+        cy.get('lib-client-item').each($client =>{
+  
+            cy.wrap($client).find('[class="network"]').then($company =>{
+                if($company.text().trim().length > 0){
+                    cy.wrap($company).should('contain',$company.text().trim()) 
+                }else{
+                    assert.fail('Manca compagnia su un elemento della pagina clients')
+                }
+            }) 
+            cy.wrap($client).find('[class="info"]').then($info =>{
+                if($info.text().trim().length > 0){
+                    cy.wrap($info).should('contain',$info.text().trim()) 
+                }else{
+                    assert.fail('Manca info a chi sono indirizzate su un elemento della pagina clients')
+                }
+            }) 
+            cy.wrap($client).find('[class="title"]').then($title =>{
+                if($title.text().trim().length > 0){
+                    cy.wrap($title).should('contain',$title.text().trim()) 
+                }else{
+                    assert.fail('Manca titolo su un elemento della pagina clients')
+                }
+            }) 
+
+        })
+
+        const suggestUltra = ['Allianz Ultra Casa e Patrimonio','Allianz Ultra Casa e Patrimonio BMP','Allianz Ultra Salute']
+        cy.get('body').find('.title').each($suggest =>{
+            expect($suggestUltra.text()).to.include(suggestUltra[i]);
+
+        })
     })
     
     it('Verifica Click su Ricerca Cliente',function(){
