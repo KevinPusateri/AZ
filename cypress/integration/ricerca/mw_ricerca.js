008ce9601a8baed8a28ad196f83c5791506c0c47
Kevin Pusateri <kevin.pusateri@allianz.it>
2021-04-06 15:35:42 +0200

    Update mw_ricerca.js

--- cypress/integration/ricerca/mw_ricerca.js
+++ cypress/integration/ricerca/mw_ricerca.js
@@ -9,6 +9,26 @@ Cypress.config('defaultCommandTimeout', 15000)
 const delayBetweenTests = 2000
 //#endregion
 
+const checkBucaRicerca = () => {
+    cy.get('input[name="main-search-input"]').click()
+    const getSection = () => cy.get('lib-shortcut-section-item')
+    getSection().find('[class="title"]:contains("Ultime pagine visitate"):visible').should('contain','Ultime pagine visitate')
+    getSection().find('[class="title"]:contains("Ultimi clienti visualizzati"):visible').should('contain','Ultimi clienti visualizzati')
+    getSection().find('[class="title"]:contains("Ultime polizze visualizzate"):visible').should('contain','Ultime polizze visualizzate')
+
+    getSection().find('lib-da-link').should('exist').and('be.visible').and('have.length', 6)
+    getSection().find('a[href^="/matrix/clients/client/"]').should('have.length', 3).and('exist').and('be.visible').and('have.attr', 'href')
+    getSection().find('img').should('have.length', 3).and('exist').and('be.visible').and('have.attr', 'src')
+    
+    getSection().find('[class="right nx-grid__column-6"]').each(($text) =>{
+        expect($text.text()).not.to.be.empty
+    })
+    getSection().find('[class="left nx-grid__column-6"]').each(($text) =>{
+        expect($text.text()).not.to.be.empty
+    })
+  }
+
+
 beforeEach(() => {
     cy.viewport(1920, 1080)
     cy.visit('https://matrix.pp.azi.allianz.it/')
@@ -46,7 +66,7 @@ describe('Matrix Ricerca', function () {
         
     })
 
-    it('Verifica Ricerca Da Landing PAge',function(){
+    it('Verifica Ricerca Da Landing Page',function(){
         const landingPage = () => cy.get('app-product-button-list').find('a')
         landingPage().contains('Clients').click()
         cy.url().should('include', '/clients')
