375d18d0f0cb24875b01134180d9ab19d9543e5a
Kevin Pusateri <kevin.pusateri@allianz.it>
2021-04-07 09:14:13 +0200

    Update mw_ricerca_risultati_clients.js

--- cypress/integration/ricerca/mw_ricerca_risultati_clients.js
+++ cypress/integration/ricerca/mw_ricerca_risultati_clients.js
@@ -57,21 +57,20 @@ describe('Buca di Ricerca - Risultati Clients', function () {
     })
 
     it('Verifica Modifica filtri',function(){
-        cy.get('input[name="main-search-input"]').click()
-        cy.get('input[name="main-search-input"]').type('AR').type('{enter}')
-        cy.url().should('include','search/clients/clients').wait(3000)
+        // cy.get('input[name="main-search-input"]').click()
+        cy.get('input[name="main-search-input"]').click().type('AR').type('{enter}')
+        cy.url().should('include','search/clients/clients').wait(5000)
 
         cy.get('.icon').find('[name="filter"]').click()
         cy.get('.filter-group').contains('Potenziale').click()
         cy.get('.filter-group').find('nx-checkbox').first().click()
         cy.get('.footer').find('button').contains('applica').click()
 
-        //TODO Finire modifica
         cy.get('[class="lib-applied-filters-item"]').find('span').should('have.length',6).each($filter =>{
             cy.wrap($filter).should('contain',$filter.text().trim())
         })
         const checkFilter = [
-            '1-30018-000 TRIESTE 2000',
+            '73-742-000 TRIESTE DIAZ',
             '73-742-060 TRIESTE DIAZ',
             'Persona fisica',
             'Persona giuridica',
@@ -79,15 +78,16 @@ describe('Buca di Ricerca - Risultati Clients', function () {
             'Cessato'
         ]
         cy.get('lib-applied-filters-item').find('span').should('have.length',6).each(($filter,i) =>{
-            cy.wrap($filter).should('contain',checkFilter[i])
+             expect($filter.text().trim()).to.include(checkFilter[i]);
         })
 
     })
 
+
     it('Verifica Click su Ricerca Cliente',function(){
         cy.get('input[name="main-search-input"]').click()
         cy.get('input[name="main-search-input"]').type('AR').type('{enter}')
-        cy.url().should('include','search/clients/clients').wait(3000)
+        cy.url().should('include','search/clients/clients').wait(5000)
         cy.get('lib-client-item').first().click()
         cy.get('app-client-profile-tabs').find('a').contains('SINTESI CLIENTE').should('have.class','active')
     })
