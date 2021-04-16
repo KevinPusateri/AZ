20e1fa937d6a1c0633780cc15fe53353d8afe0f5
Andrea Oboe <andrea.oboe@allianz.it>
2021-04-16 11:23:57 +0200

    Update dash_full.bat

--- scripts/dash_full.bat
+++ scripts/dash_full.bat
@@ -1,4 +1,2 @@
 cd ..
-npm run cypress:run:ricerca_dash
-npm run cypress:run:navigations_dash
-npm run cypress:run:censimenti_dash
+npm-run-all -s -c cypress:run:ricerca_dash cypress:run:navigations_dash npm run cypress:run:censimenti_dash
