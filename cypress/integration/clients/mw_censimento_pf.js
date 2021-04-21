1ea83a1e63d1c4b28ec5f520b3113baa61f587a8
Andrea Oboe <andrea.oboe@allianz.it>
2021-04-21 09:29:04 +0200

    Update send_screenshot script

--- cypress/integration/clients/mw_censimento_pf.js
+++ cypress/integration/clients/mw_censimento_pf.js
@@ -77,7 +77,7 @@ before(() => {
 
   cy.visit('https://matrix.pp.azi.allianz.it/')
   cy.get('input[name="Ecom_User_ID"]').type(currentUser)
-  cy.get('input[name="Ecom_Password"]').type('P@ssw0rd!fff')
+  cy.get('input[name="Ecom_Password"]').type('P@ssw0rd!')
   cy.get('input[type="SUBMIT"]').click()
   cy.url().should('include','/portaleagenzie.pp.azi.allianz.it/matrix/')
 
