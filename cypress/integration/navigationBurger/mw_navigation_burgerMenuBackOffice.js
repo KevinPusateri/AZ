ed0e5d91e3b9d6649161fe2b5790fa293a4ae435
Kevin Pusateri <kevin.pusateri@allianz.it>
2021-05-27 16:35:15 +0200

    Update mw_navigation_burgerMenuBackOffice.js

--- cypress/integration/navigationBurger/mw_navigation_burgerMenuBackOffice.js
+++ cypress/integration/navigationBurger/mw_navigation_burgerMenuBackOffice.js
@@ -37,44 +37,44 @@ describe('Matrix Web : Navigazioni da Burger Menu in Backoffice', function () {
 
     it('Verifica link da Burger Menu', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.checkLinksBurgerMenu()
+        BurgerMenuBackOffice.checkExistLinks()
     });
 
     //#region Sinistri
-    it.only('Verifica aggancio Movimentazione sinistri', function () {
+    it('Verifica aggancio Movimentazione sinistri', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Movimentazione sinistri')
+        BurgerMenuBackOffice.clickLink('Movimentazione sinistri')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
 
     it('Verifica aggancio Denuncia', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Denuncia')
+        BurgerMenuBackOffice.clickLink('Denuncia')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Denuncia BMP', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Denuncia BMP')
+        BurgerMenuBackOffice.clickLink('Denuncia BMP')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Consultazione sinistri', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Consultazione sinistri')
+        BurgerMenuBackOffice.clickLink('Consultazione sinistri')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Sinistri incompleti', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Sinistri incompleti')
+        BurgerMenuBackOffice.clickLink('Sinistri incompleti')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Sinistri canalizzati', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Sinistri canalizzati')
+        BurgerMenuBackOffice.clickLink('Sinistri canalizzati')
         BurgerMenuBackOffice.backToBackOffice()
     })
     //#endregion
@@ -82,73 +82,73 @@ describe('Matrix Web : Navigazioni da Burger Menu in Backoffice', function () {
     //#region Contabilità
     it('Verifica aggancio Sintesi Contabilità', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Sintesi Contabilità')
+        BurgerMenuBackOffice.clickLink('Sintesi Contabilità')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Giornata contabile', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Giornata contabile')
+        BurgerMenuBackOffice.clickLink('Giornata contabile')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Consultazione Movimenti', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Consultazione Movimenti')
+        BurgerMenuBackOffice.clickLink('Consultazione Movimenti')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Estrazione Contabilità', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Estrazione Contabilità')
+        BurgerMenuBackOffice.clickLink('Estrazione Contabilità')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Deleghe SDD', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Deleghe SDD')
+        BurgerMenuBackOffice.clickLink('Deleghe SDD')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Quadratura unificata', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Quadratura unificata')
+        BurgerMenuBackOffice.clickLink('Quadratura unificata')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Incasso per conto', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Incasso per conto')
+        BurgerMenuBackOffice.clickLink('Incasso per conto')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Incasso massivo', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Incasso massivo')
+        BurgerMenuBackOffice.clickLink('Incasso massivo')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Sollecito titoli', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Sollecito titoli')
+        BurgerMenuBackOffice.clickLink('Sollecito titoli')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Impostazione contabilità', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Impostazione contabilità')
+        BurgerMenuBackOffice.clickLink('Impostazione contabilità')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Convenzioni in trattenuta', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Convenzioni in trattenuta')
+        BurgerMenuBackOffice.clickLink('Convenzioni in trattenuta')
         BurgerMenuBackOffice.backToBackOffice()
     })
 
     it('Verifica aggancio Monitoraggio Customer Digital Footprint', function () {
         TopBar.clickBackOffice()
-        BurgerMenuBackOffice.clickLinkOnBurgerMenu('Monitoraggio Customer Digital Footprint')
+        BurgerMenuBackOffice.clickLink('Monitoraggio Customer Digital Footprint')
         BurgerMenuBackOffice.backToBackOffice()
 
     })
