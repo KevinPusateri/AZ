68a5bc1d44974b1bfad7f827b86e74021e5e5c10
Andrea Oboe <andrea.oboe@allianz.it>
2021-07-20 13:53:19 +0200

    Update send_screenshot.js
    Set new mail server : techuser.mail.allianz

--- scripts/send_screenshot.js
+++ scripts/send_screenshot.js
@@ -49,25 +49,24 @@ const sendMail = async () => {
 		to: htmlExportLogMailTo,
 		subject: mailSubject,
 		text: mailSubject,
-		html: '<b>Report ' + mailSubject + '</b></br></br>For additional info, write to andrea.oboe@allianz.it or kevin.pusateri@allianz.it</br></br>'
-		// attachments: [
-		// 	{
-		// 		filename: 'MW_FE_' + stream.toUpperCase() + '_PREPROD.zip',
-		// 		path: '..//MW_FE_' + stream.toUpperCase() + '_PREPROD.zip'
+		html: '<b>Report ' + mailSubject + '</b></br></br>For additional info, write to andrea.oboe@allianz.it or kevin.pusateri@allianz.it</br></br>',
+		attachments: [
+			{
+				filename: 'MW_FE_' + stream.toUpperCase() + '_PREPROD.zip',
+				path: '..//MW_FE_' + stream.toUpperCase() + '_PREPROD.zip'
 
-		// 	}
-		// ]
+			}
+		]
 	});
 }
 
 async function main() {
-	await sendMail();
-	// if (fs.existsSync(dirLogs)) {
-	// 	await zipDirectory(dirLogs, '..//MW_FE_' + stream.toUpperCase() + '_PREPROD.zip');
-	// 	await sendMail();
-	// }
-	// else
-	// 	console.log('Nothing to send => NO ERRORS :) ')
+	if (fs.existsSync(dirLogs)) {
+		await zipDirectory(dirLogs, '..//MW_FE_' + stream.toUpperCase() + '_PREPROD.zip');
+		await sendMail();
+	}
+	else
+		console.log('Nothing to send => NO ERRORS :) ')
 }
 
 main();
