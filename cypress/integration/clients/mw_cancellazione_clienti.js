552e739b90fc98d7e8d8f2c8e01f291d79f979ec
Kevin Pusateri <kevin.pusateri@allianz.it>
2021-08-03 14:57:03 +0200

    Prova Wait messaggio

--- cypress/integration/clients/mw_cancellazione_clienti.js
+++ cypress/integration/clients/mw_cancellazione_clienti.js
@@ -64,6 +64,7 @@ describe('Matrix Web - Hamburger Menu: Cancellazione Clienti ', function () {
     SCUCancellazioneClienti.eseguiCancellazioneOnPersonaFisica().then(currentClient => {
       cliente = currentClient
     })
+    cy.wait(200)
   })
 
   it('Ricercare i clienti in buca di ricerca - accedere alla scheda', function () {
