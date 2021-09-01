8a8867ea48639eefdd6b63e80591e71d6c323512
chiara-costa <chiara.costa@allianz.it>
2021-09-01 16:42:39 +0200

    motor

--- cypress/mw_page_objects/Motor/PreventivoMotor.js
+++ cypress/mw_page_objects/Motor/PreventivoMotor.js
@@ -12,12 +12,27 @@ require('cypress-plugin-tab')
 
 class PreventivoMotor {
 
-    static compilaDatiQuotazione(targa,dataNascita) {
-        getIFrame().find('input[id="nx-input-0"]').should('exist').and('be.visible').type(targa);
-        getIFrame().find('input[id="nx-input-1"]').should('exist').and('be.visible').type(dataNascita)
-       getIFrame().find('nx-checkbox[id="informativa"]').should('exist').and('be.visible').check()
+    static compilaDatiQuotazione(targa, dataNascita) {
+        cy.getIFrame()
+        cy.get('@iframe').within(() => {
+        cy.get('nx-checkbox[id="informativa"]').should('exist').and('be.visible').within(() => {
+        cy.get('span[class="nx-checkbox__control"]').click()
+        })
+        cy.get('input[id="nx-input-1"]').should('exist').and('be.visible')
+        cy.get('input[ng-reflect-name="DataNascitaProprietario"]').type(dataNascita)
+        cy.get('input[aria-label="Targa"]').should('exist').and('be.visible').type(targa);
+        cy.get('input[aria-label="Targa"]').should('exist').and('be.visible').type(targa);
+        //cy.get('input[ng-reflect-name="check"]').check();
+        cy.contains('Calcola').should('be.visible')
+        cy.contains('Calcola').click({force:true})
+
         
-    }
+        cy.get('input[ng-reflect-name]').should('be.visible').type('mazzini')
+        //cy.get('input[nx-input-9]').type('1')
+        //cy.get('input[nx-input-9]').type('Monfalcone')
+
+        })
+        }
   }
   
   
