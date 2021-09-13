7ddf4d087963a77723a1f7e6a663ec44679b4b5b
Kevin Pusateri <kevin.pusateri@allianz.it>
2021-09-13 11:19:55 +0200

    fixed convenzioni login

--- cypress/integration/clients/mw_convenzioni.js
+++ cypress/integration/clients/mw_convenzioni.js
@@ -106,7 +106,7 @@ describe('Matrix Web : Convenzioni', {
                 cy.log('Retrived party relation : ' + currentClient[1].name + ' ' + currentClient[1].firstName)
                 retrivedClient = currentClient[0]
                 retrivedPartyRelations = currentClient[1]
-                LoginPage.logInMW('TUTF003', psw)
+                LoginPage.logInMW('TUTF003', psw,true,'010745000')
                 SintesiCliente.visitUrlClient(currentClient[0].customerNumber, false)
                 DettaglioAnagrafica.clickTabDettaglioAnagrafica()
                 DettaglioAnagrafica.clickSubTab('Convenzioni')
