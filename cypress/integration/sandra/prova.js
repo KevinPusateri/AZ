00de4f617aad65eab9e98fcf1177d1c3acc6313a
Sandra <sandra.marina.espeche@allianz.it>
2021-09-24 13:42:11 +0200

    Update prova.js

--- cypress/integration/sandra/prova.js
+++ cypress/integration/sandra/prova.js
@@ -11,8 +11,8 @@ import LandingRicerca from "../../mw_page_objects/ricerca/LandingRicerca"
 import DatiPreventivo from "../../mw_page_objects/Motor/DatiPreventivo"
 
 //#region Username Variables
-const userName = 'TUTF021'   // 'TUTF021'
-const psw = 'P@ssw0rd!'
+//const userName = 'TUTF017'   // 'TUTF021'
+//const psw = 'P@ssw0rd!'
 //#endregion
 
 //#region Mysql DB Variables
@@ -28,20 +28,18 @@ Cypress.config('defaultCommandTimeout', 60000)
 
 
 before(() => {
-    cy.task('startMysql', { dbConfig: dbConfig, testCaseName: testName, currentEnv: currentEnv, currentUser: userName }).then((results) => {
-        insertedId = results.insertId
-    })
-    LoginPage.logInMW(userName, psw)
-})
-
-beforeEach(() => {
     cy.getUserWinLogin().then(data=> {
-        cy.task(‘startMysql’,  { dbConfig: dbconfig, testCaseName: testName, current 
+        cy.task('startMysql',  { dbConfig: dbconfig, testCaseName: testName, current 
             Env: currentEnv, currentUser: data.tutf}).then((results) => {
                 insertedId=results.insertId
             })
             LoginPage.logInMWAdvanced()
         })
+   // LoginPage.logInMW(userName, psw)
+})
+
+beforeEach(() => {
+   
     cy.preserveCookies()
     Common.visitUrlOnEnv()
 })
