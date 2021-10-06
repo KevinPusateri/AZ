10389fee1ff84a7e9ecdc0d18609ff9ada6437a1
Andrea Oboe <andrea.oboe@allianz.it>
2021-10-06 11:49:59 +0200

    New PDF procedure

--- cypress/integration/checkPDF.js
+++ cypress/integration/checkPDF.js
@@ -3,77 +3,16 @@
  */
 
 /// <reference types="Cypress" />
-/// <reference types="cypress-downloadfile"/>
 //#region Configuration
 Cypress.config('defaultCommandTimeout', 60000)
 
 
 describe('Download File and Assert the content', () => {
-    // it('Download & Extract Text', () => {
-    //     cy.downloadFile('https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg', 'mydownloads', 'example.jpg')
-    //         .then(() => {
-    //             cy.task("getImageText", { fileName: "./mydownloads/example.jpg", lang: "eng", logger: true })
-    //                 .then(text => {
-    //                     expect(text).to.contains("Are you enjoying Cypress")
-    //                 })
-    //         })
-    // });
-
-    // it('Download & Compare Images', () => {
-    //     cy.downloadFile('https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg', 'mydownloads', 'example.jpg')
-    //         .then(() => {
-    //             cy.task("compareImages", { fileName1: "./mydownloads/example.jpg", fileName2: "DifferentImage.jpg" })
-    //                 .then(isMatching => {
-    //                     expect(isMatching).to.be.true;
-    //                 })
-    //         })
-    // });
 
 
-
-    // it('tests a pdf', () => {
-    //     cy.task('getPdfContent', 'resocontoPulini.pdf').then(content => {
-    //         console.log(content)
-    //         cy.fixture('resocontoPulini.json').then((data) => {
-    //             console.log(data)
-    //         })
-    //         cy.task('getPdfContent', 'resocontoSantucci.pdf').then(content => {
-    //             cy.fixture('resocontoSantucci.json').then((data) => {
-    //                 cy.readFile('cypress/fixtures/resocontoPulini.json').then(pulini => {
-    //                     cy.readFile('cypress/fixtures/resocontoSantucci.json').then(santucci => {
-    //                         var textPulini = pulini.formImage.Pages[0].Texts
-    //                         var textSantucci = pulini.formImage.Pages[0].Texts
-    //                         console.log(pulini)
-    //                         // console.log(textSantucci)
-    //                         // assert.include(TextPulini,TextSantucci)
-    //                     })
-    //                 })
-    //             })
-    //         })
-    //     })
-    // }) 
-
-
-    it.only('tests a pdf', () => {
-        cy.task('getPdfContentProva', 'pdf/resocontoPulini.pdf').then(pulini => {
-            var testoPulini = pulini.text.replaceAll(': ','').replaceAll(':','').split('\n')
-            // if (testoPulini.includes('Codice Fiscale: ')) {
-                //     assert.isOk('Bravo')
-                // }
-                // else
-                //     assert.fail('NOOO')
-                cy.task('getPdfContentProva', 'pdf/resocontoSantucci.pdf').then(santucci => {
-                    var testoSantucci = santucci.text.replaceAll(': ','').replaceAll(':','').split('\n')
-                    console.log(testoSantucci)
-                    // var testoSantucci =santucci.text
-                expect(testoPulini).to.match(/Codice Fiscale/)
-                expect(testoPulini).to.match(/Nascita/)
-                expect(testoPulini).to.match(/Residenza/)
-                expect(testoPulini).to.match(/Doc.Personale/)
-                expect(testoPulini).to.match(/Professione/)
-                expect(testoPulini).to.match(/Telefono fisso/)
-                expect(testoPulini).to.match(/Mobile/)
-            })
+    it('tests a pdf', () => {
+        cy.parsePdf().then(data => {
+            cy.log(data)
         })
     })
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,6 +13,7 @@ const moment = require('moment')
 const os = require('os')
 const CryptoJS = require('crypto-js')
 
+
 //
 //
 // -- This is a parent command --
@@ -813,4 +814,35 @@ Cypress.Commands.add('selfWindow', () => {
   });
 })
 
+// Performs an XMLHttpRequest instead of a cy.request (able to send data as 
+// FormData - multipart/form-data)
+Cypress.Commands.add('form_request', (method, url, formData, done) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onload = function () {
+    done(xhr);
+  };
+  xhr.onerror = function () {
+    done(xhr);
+  };
+  xhr.send(formData);
+})
 
+
+
+/**
+ * 
+ */
+Cypress.Commands.add('parsePdf', () => {
+
+  const formData = new FormData()
+  formData.append('file', 'D:\\TA\\matrix-web-fe-tests\\cypress\\fixtures\\Autocertificazione_Test.pdf')
+
+  const method = 'POST'
+  const url = 'http://' + Cypress.env('hostParsr') + ':' + Cypress.env('portParsr') + '/api/v1/document'
+  cy.form_request(method, url, formData, function (response) {
+    console.log(response)
+    expect(response.status).to.eq(200);
+    expect(expectedAnswer).to.eq(response.response);
+  });
+})
