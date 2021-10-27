748a89264f4f2a8088e7bb7241b43f6804e61eb8
Kevin Pusateri <kevin.pusateri@allianz.it>
2021-10-27 15:27:28 +0200

    utenza pietro

---
+++ cypress/integration/UltraBMP/ub_aggiunta_fabbricato.js
@@ -0,0 +1,52 @@
+/**
+ * @author Kevin Pusateri <kevin.pusateri@allianz.it>
+ */
+
+/// <reference types="Cypress" />
+import Common from "../../mw_page_objects/common/Common"
+import LoginPage from "../../mw_page_objects/common/LoginPage"
+import TopBar from "../../mw_page_objects/common/TopBar"
+
+//#region Mysql DB Variables
+const testName = Cypress.spec.name.split('/')[1].split('.')[0].toUpperCase()
+const currentEnv = Cypress.env('currentEnv')
+const dbConfig = Cypress.env('db')
+let insertedId
+    //#endregion
+
+//#region Configuration
+Cypress.config('defaultCommandTimeout', 60000)
+
+//#endregion
+
+
+before(() => {
+    cy.getUserWinLogin().then(data => {
+        cy.startMysql(dbConfig, testName, currentEnv, data).then((id) => insertedId = id)
+        LoginPage.logInMWAdvanced()
+    })
+})
+
+beforeEach(() => {
+    cy.preserveCookies()
+    Common.visitUrlOnEnv(false, false)
+})
+
+after(function() {
+    TopBar.logOutMW()
+        //#region Mysql
+    cy.getTestsInfos(this.test.parent.suites[0].tests).then(testsInfo => {
+            let tests = testsInfo
+            cy.finishMysql(dbConfig, insertedId, tests)
+        })
+        //#endregion
+})
+
+describe('Matrix Web : Navigazioni da BackOffice', function() {
+
+    it('dasdasdas', () => {
+
+    })
+
+
+})
