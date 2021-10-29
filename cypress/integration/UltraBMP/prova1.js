f1c7ff6e391cac727ed3235fba3d8c07c776de15
Pietro Scocchi <pietro.scocchi@allianz.it>
2021-10-29 10:52:05 +0200

    Prime prove UltraBMP
    Prime prove UltraBMP

--- cypress/integration/UltraBMP/prova1.js
+++ cypress/integration/UltraBMP/prova1.js
@@ -1,8 +1,8 @@
 /**
  * @author Pietro Scocchi <pietro.scocchi@allianz.it>
- */
-
+*/
 /// <reference types="Cypress" />
+
 import UltraBMP from "../../mw_page_objects/UltraBMP/UltraBMP"
 import Common from "../../mw_page_objects/common/Common"
 import LoginPage from "../../mw_page_objects/common/LoginPage"
@@ -14,60 +14,42 @@ const testName = Cypress.spec.name.split('/')[1].split('.')[0].toUpperCase()
 const currentEnv = Cypress.env('currentEnv')
 const dbConfig = Cypress.env('db')
 let insertedId
-    //#endregion
+//#endregion
 
 //#region Configuration
 Cypress.config('defaultCommandTimeout', 60000)
 
 //#endregion
 
-
 before(() => {
     cy.getUserWinLogin().then(data => {
-        cy.startMysql(dbConfig, testName, currentEnv, data).then((id) => insertedId = id)
+        cy.startMysql(dbConfig, testName, currentEnv, data).then((id)=> insertedId = id )
         LoginPage.logInMWAdvanced()
     })
 })
 
 beforeEach(() => {
     cy.preserveCookies()
+    Common.visitUrlOnEnv()
 })
 
-after(function() {
+after(function () {
     TopBar.logOutMW()
-        //#region Mysql
+    //#region Mysql
     cy.getTestsInfos(this.test.parent.suites[0].tests).then(testsInfo => {
-            let tests = testsInfo
-            cy.finishMysql(dbConfig, insertedId, tests)
-        })
-        //#endregion
+        let tests = testsInfo
+        cy.finishMysql(dbConfig, insertedId, tests)
+    })
+    //#endregion
+
 })
 
-describe('Matrix Web : Navigazioni da BackOffice', function() {
+
+describe('Matrix Web : Navigazioni da Sales', function () {
 
     it('Seleziona Ultra BMP', () => {
         TopBar.clickSales()
         BurgerMenuSales.clickLink('Allianz Ultra Casa e Patrimonio BMP')
     })
-     
-    /*
-    it('Quotazione', () => {
-        cy.getIFrame()
-        cy.get('@iframe').within(() => {
-            
-            cy.get('div[class="nx-dropdown__container"]').first().then(($div)=>{
-                cy.get('span').contains('appartamento').click()
-            })
 
-            //cy.get('div[class="nx-dropdown__container"]').first().find('span').contains('appartamento').should(be.visible)
-            
-        })
-    })
-    */
-    
-    
-    it("Imposta valori quotazione", () => {
-        UltraBMP.compilaDatiQuotazione()
-    })
-    
-})
+});
