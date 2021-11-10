08183ab1de55aa1bd2b2aed7958baec020eb038a
Andrea Oboe <andrea.oboe@allianz.it>
2021-11-10 17:03:17 +0100

    Continuazione NGRA2021 per Tenuta Tariffa : atterraggio fino a pagina Offerta

--- cypress/mw_page_objects/tenutaTariffa/TenutaTariffa.js
+++ cypress/mw_page_objects/tenutaTariffa/TenutaTariffa.js
@@ -31,10 +31,11 @@ class TenutaTariffa {
             cy.get('input[aria-label="Targa"]').type(currentCase.Targa).wait(500)
 
             //Data di Nascita : calcolata in automatico a partire dalla data decorrenza in rapporto all'età del caso
+            debugger
             let dataDecorrenza = calcolaDataDecorrenza(currentCase)
             currentDataNascita = new Date(dataDecorrenza.getFullYear() - currentCase.Eta, dataDecorrenza.getMonth(), dataDecorrenza.getDay())
-            let formattedDataNascita = String(currentDataNascita.getDay()).padStart(2, '0') + '/' +
-                String(currentDataNascita.getMonth() + 1).padStart(2, '0') + '/' +
+            let formattedDataNascita = String(currentDataNascita.getDay()) + '/' +
+                String(currentDataNascita.getMonth() + 1) + '/' +
                 currentDataNascita.getFullYear()
             cy.get('input[nxdisplayformat="DD/MM/YYYY"]').should('exist').and('be.visible').click().wait(500)
             cy.get('input[nxdisplayformat="DD/MM/YYYY"]').type(formattedDataNascita).wait(1000)
@@ -100,7 +101,6 @@ class TenutaTariffa {
             url: '**/assuntivomotor/**'
         }).as('getMotor')
 
-        debugger
         cy.getIFrame()
         cy.get('@iframe').within(() => {
             //Data Immatricolazione
@@ -135,20 +135,142 @@ class TenutaTariffa {
             //Modello
             cy.get('nx-dropdown[formcontrolname="modello"]').should('exist').and('be.visible').click().wait(500)
             cy.get('.nx-dropdown__filter-input').should('exist').and('be.visible').type(currentCase.Modello).wait(500)
-            re = new RegExp("\^ " + currentCase.Modello + " \$")
-            cy.contains(re).should('exist').and('be.visible').click().wait(500)
+            cy.contains(currentCase.Modello).should('exist').and('be.visible').click().wait(500)
             cy.wait('@getMotor', { requestTimeout: 30000 })
 
             //Allestimento
             cy.get('nx-dropdown[formcontrolname="versione"]').should('exist').and('be.visible').click().wait(500)
-            cy.get('.nx-dropdown__filter-input').should('exist').and('be.visible').type(currentCase.Version).wait(500)
-            re = new RegExp("\^ " + currentCase.Versione + " \$")
-            cy.contains(re).should('exist').and('be.visible').click().wait(500)
+            cy.get('.nx-dropdown__filter-input').should('exist').and('be.visible').type(currentCase.Versione).wait(500)
+            cy.contains(currentCase.Versione).should('exist').and('be.visible').click().wait(500)
             cy.wait('@getMotor', { requestTimeout: 30000 })
 
-            debugger
+            //Posti
+            cy.get('input[formcontrolname="posti"]').should('exist').and('be.visible').type(currentCase.Posti).type('{enter}').wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+            cy.contains('AVANTI').should('exist').and('be.visible').click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
 
         });
+    }
+
+    static compilaProvenienza(currentCase) {
+        cy.intercept({
+            method: '+(GET|PUT)',
+            url: '**/assuntivomotor/**'
+        }).as('getMotor')
+
+        cy.getIFrame()
+        cy.get('@iframe').within(() => {
+            cy.wait(2000)
+            //Provenienza
+            cy.get('nx-dropdown[aria-haspopup="listbox"]').first().should('be.visible').click().wait(500)
+            cy.get('nx-dropdown-item').should('be.visible').contains(currentCase.Provenienza).click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+            //Nel Dettaglio
+            cy.get('nx-dropdown[aria-haspopup="listbox"]').last().should('be.visible').click().wait(500)
+            cy.get('nx-dropdown-item').should('be.visible').contains(currentCase.Nel_Dettaglio).click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+
+            cy.contains('INSERISCI').should('exist').and('be.visible').click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+
+            //Popup di dichiarazione di non circolazione a SI
+            //!ATTENZIONE CHE AL MOMENTO COMPARE DUE VOLTE
+            cy.contains('Si').should('exist').and('be.visible').parent().click().wait(500)
+            cy.contains('CONTINUA').should('exist').and('be.visible').click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+            cy.wait(1000)
+
+            cy.contains('Si').should('exist').and('be.visible').parent().click().wait(500)
+            cy.contains('CONTINUA').should('exist').and('be.visible').click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+
+            //Nessuna attestazione trovata in BDA
+            cy.contains('Si').should('exist').and('be.visible').parent().click().wait(500)
+            cy.contains('CONTINUA').should('exist').and('be.visible').click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+            //Scadenza precedente contratto
+            let dataScadenzaPrecedenteContratto
+            let dataDecorrenza = calcolaDataDecorrenza(currentCase)
+            if (currentCase.Data_Scadenza.split(' ')[1].includes('giorn'))
+                dataScadenzaPrecedenteContratto = new Date(dataDecorrenza.getFullYear(),
+                    dataDecorrenza.getMonth(),
+                    dataDecorrenza.getDay() - currentCase.Data_Scadenza.split(' ')[0])
+            else if (currentCase.Data_Scadenza.split(' ')[1].includes('mes'))
+                dataScadenzaPrecedenteContratto = new Date(dataDecorrenza.getFullYear(),
+                    dataDecorrenza.getMonth() - currentCase.Data_Scadenza.split(' ')[0],
+                    dataDecorrenza.getDay())
+
+            let formattedDataScadenzaPrecedenteContratto = String(dataScadenzaPrecedenteContratto.getDay()).padStart(2, '0') + '/' +
+                String(dataScadenzaPrecedenteContratto.getMonth() + 1).padStart(2, '0') + '/' +
+                dataScadenzaPrecedenteContratto.getFullYear()
+
+            cy.get('input[nxdisplayformat="DD/MM/YYYY"]').should('exist').and('be.visible').clear().wait(500)
+            cy.get('input[nxdisplayformat="DD/MM/YYYY"]').should('exist').and('be.visible').type(formattedDataScadenzaPrecedenteContratto).type('{enter}').wait(500)
+
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+            //Compagnia di provenienza
+            cy.contains('COMPAGNIA DI PROVENIENZA').parents('div[class="nx-formfield__input"]').find('nx-dropdown').should('be.visible').click().wait(500)
+            cy.get('nx-dropdown-item').contains(currentCase.Compagnia_Provenienza).click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+            //Formula di provenienza
+            cy.contains('FORMULA DI PROVENIENZA').parents('div[class="nx-formfield__input"]').find('nx-dropdown').should('be.visible').click().wait(500)
+            cy.get('nx-dropdown-item').contains(currentCase.Formula_Provenienza).click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+
+            //CL B. provenienza
+            cy.contains('CL. B/ M provenienza').parents('div[class="nx-formfield__input-container"]').find('input').should('be.visible').type(currentCase.Cl_BM_Provenienza).wait(500)
+            //CL B. assegnazione
+            cy.contains('Cl. B/ M assegnazione').parents('div[class="nx-formfield__input-container"]').find('input').should('be.visible').type(currentCase.Cl_BM_Assegnazione).wait(500)
+            //CL provenienza CU
+            cy.contains('Cl. provenienza cu').parents('div[class="nx-formfield__input"]').find('nx-dropdown').should('be.visible').click().wait(500)
+            cy.get('nx-dropdown-item').contains(currentCase.Cl_Provenienza_CU).click().wait(500)
+            //CL assegnazione CU
+            cy.contains('Cl. assegnazione cu').parents('div[class="nx-formfield__input"]').find('nx-dropdown').should('be.visible').click().wait(500)
+            cy.get('nx-dropdown-item').contains(currentCase.Cl_Assegnazione_CU).click().wait(500)
+
+            //TODO Attestato conforme all'articolo 134, comma 4 bis, del Codice assicurazioni ?
+
+            cy.contains('AVANTI').should('exist').and('be.visible').click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 100000 })
+        })
+    }
+
+    static compilaOfferta(currentCase) {
+        cy.intercept({
+            method: '+(GET|PUT)',
+            url: '**/assuntivomotor/**'
+        }).as('getMotor')
+
+        cy.getIFrame()
+        cy.get('@iframe').within(() => {
+            //Data Decorrenza
+                let dataDecorrenza = calcolaDataDecorrenza(currentCase)
+                debugger
+                let formattedDataDecorrenza = String(dataDecorrenza.getDay()).padStart(2, '0') + '/' +
+                    String(dataDecorrenza.getMonth() + 1).padStart(2, '0') + '/' +
+                    dataDecorrenza.getFullYear()
+
+                cy.get('input[formcontrolname="dataDecorrenza"]').should('exist').and('be.visible').clear().wait(500)
+                cy.get('input[formcontrolname="dataDecorrenza"]').should('exist').and('be.visible').type(formattedDataDecorrenza).type('{enter}').wait(500)
+
+                cy.wait('@getMotor', { requestTimeout: 30000 })
+
+            //Conferma
+
+            //Frazionamento
+            cy.get('nx-dropdown[formcontrolname="frazionamento"]').should('exist').and('be.visible').click().wait(1000)
+            cy.contains(currentCase.Frazionamento.toUpperCase()).should('exist').and('be.visible').click().wait(500)
+            cy.wait('@getMotor', { requestTimeout: 30000 })
+        })
     }
 }
 
