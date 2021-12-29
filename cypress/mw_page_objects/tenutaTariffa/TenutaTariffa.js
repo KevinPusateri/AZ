005db7ec84a0148fe5424ee38b478aac25604ffe
Andrea Oboe <andrea.oboe@allianz.it>
2021-12-29 13:06:31 +0100

    Fixed Tenuta Tariffa Veicolo

--- cypress/mw_page_objects/tenutaTariffa/TenutaTariffa.js
+++ cypress/mw_page_objects/tenutaTariffa/TenutaTariffa.js
@@ -268,7 +268,6 @@ class TenutaTariffa {
                 cy.get('input[formcontrolname="marcaModelloVersione"]').should('exist').type(currentCase.Marca + ' ' + currentCase.Modello + ' ' + currentCase.Versione)
             }
             else {
-                //Su AVIVA
                 //Marca
                 cy.get('nx-dropdown[formcontrolname="marca"]').should('exist').and('be.visible').click()
                 cy.get('.nx-dropdown__filter-input').should('exist').and('be.visible').type(currentCase.Marca)
@@ -276,9 +275,25 @@ class TenutaTariffa {
                 cy.contains(re).should('exist').and('be.visible').click()
                 //Attendiamo che il caricamento non sia più visibile
                 cy.get('nx-spinner').should('not.be.visible')
-                
-                //Modello Versione testo libero
-                cy.get('input[formcontrolname="marcaModelloVersione"]').should('exist').type(currentCase.Marca + ' ' + currentCase.Modello + ' ' + currentCase.Versione)
+
+                //Per i modelli fuori catalogo, da compilare a mano; altrimenti utilizzo i dropdown
+                if (currentCase.Settore === '3') {
+                    //Modello Versione testo libero (essendo fuori catalogo)
+                    cy.get('input[formcontrolname="marcaModelloVersione"]').should('exist').type(currentCase.Marca + ' ' + currentCase.Modello + ' ' + currentCase.Versione)
+                }
+                else {
+                    //Modello
+                    cy.get('nx-dropdown[formcontrolname="modello"]').should('exist').and('be.visible').click()
+                    cy.get('.nx-dropdown__filter-input').should('exist').and('be.visible').type(currentCase.Modello)
+                    cy.contains(currentCase.Modello).should('exist').and('be.visible').click()
+                    cy.wait('@getMotor', { requestTimeout: 30000 })
+
+                    //Allestimento
+                    cy.get('nx-dropdown[formcontrolname="versione"]').should('exist').and('be.visible').click()
+                    cy.get('.nx-dropdown__filter-input').should('exist').and('be.visible').type(currentCase.Versione)
+                    cy.contains(currentCase.Versione).should('exist').and('be.visible').click()
+                    cy.wait('@getMotor', { requestTimeout: 30000 })
+                }
             }
 
             currentCase.Targa !== '' ? cy.contains('Informazioni Generali').click() : cy.contains('Ricerca in banche dati il veicolo tramite il numero di targa o il modello prima di procedere all’inserimento.').click()
@@ -555,8 +570,7 @@ class TenutaTariffa {
                     break
                 case '3':
                     //Franchigia
-                    if(currentCase.Franchigia !== '')
-                    {
+                    if (currentCase.Franchigia !== '') {
                         cy.get(':contains("Franchigia"):last').parents('motor-form-controllo').find('nx-dropdown').should('be.visible').click()
                         cy.get('nx-dropdown-item').contains(currentCase.Franchigia).click()
                         cy.wait('@getMotor', { requestTimeout: 30000 })
@@ -567,8 +581,7 @@ class TenutaTariffa {
                     //? Conducente/Tipo Guida è presettato
 
                     //Danni alle cose dei terzi trasportati
-                    if(currentCase.Danni_Terzi_Trasportati !== '')
-                    {
+                    if (currentCase.Danni_Terzi_Trasportati !== '') {
                         cy.contains('Danni alle cose dei terzi trasportati').parents('motor-form-controllo').find('nx-dropdown').should('be.visible').click()
                         cy.get('nx-dropdown-item').contains(currentCase.Danni_Terzi_Trasportati).click()
                         //Attendiamo che il caricamento non sia più visibile
@@ -577,8 +590,7 @@ class TenutaTariffa {
                     }
 
                     //Rinuncia rilvalsa
-                    if(currentCase.Rinuncia_Rivalsa !== '')
-                    {
+                    if (currentCase.Rinuncia_Rivalsa !== '') {
                         cy.contains('Rivalsa').parents('motor-form-controllo').should('be.visible').find('nx-dropdown').click()
                         cy.get('nx-dropdown-item').contains(currentCase.Rinuncia_Rivalsa).click()
                         cy.wait('@getMotor', { requestTimeout: 30000 })
