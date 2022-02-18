cff20ecc3598ad83655a4425b1287e0a8fe30aaf
Chiara <chiara.costa@allianz.it>
2022-02-18 08:44:12 +0100

    beneficiari

--- cypress/mw_page_objects/UltraBMP/Dashboard.js
+++ cypress/mw_page_objects/UltraBMP/Dashboard.js
@@ -89,9 +89,7 @@ class Dashboard {
 
     static aggiungiAmbito(nuovoAmbito) {
         ultraIFrame().within(() => {
-
             cy.get('nx-icon[class*="' + nuovoAmbito + '"]').parents('ultra-ambito-button').contains("+ Aggiungi nuovo").click()
-
             cy.wait(500)
 
         })
