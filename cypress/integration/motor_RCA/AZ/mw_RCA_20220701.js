0e4b24a9beacb674feeec4d6413438fef6baf29a
Andrea Oboe <andrea.oboe@allianz.it>
2022-04-29 16:36:46 +0200

    Remove selectedSettori

--- cypress/integration/motor_RCA/AZ/mw_RCA_20220701.js
+++ cypress/integration/motor_RCA/AZ/mw_RCA_20220701.js
@@ -56,7 +56,7 @@ let flowClients = false
 //?Se specificato, esegue i test per i casi specificati (inserirli in formato stringa)
 let caseToExecute = []
 //?Se specificato, esegue i test per i settori indicati (inserirli in formato stringa)
-let selectedSettori = ['']
+let selectedSettori = []
 
 describe('RCA Luglio 2022: ', {
     retries: {
