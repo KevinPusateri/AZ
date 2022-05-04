89e28f6f6a3746360a3c774a9958cc69ab89a4d0
Andrea Oboe <andrea.oboe@allianz.it>
2022-05-04 17:00:08 +0200

    :hammer: Start #162

--- cypress/mw_page_objects/cm/CampagneCommerciali.js
+++ cypress/mw_page_objects/cm/CampagneCommerciali.js
@@ -2,6 +2,18 @@
 
 import Common from "../common/Common"
 
+//#region iFrame
+const getIFrame = () => {
+    cy.get('iframe[class="iframe-content ng-star-inserted"]')
+        .iframe();
+
+    let iframeSCU = cy.get('iframe[class="iframe-content ng-star-inserted"]')
+        .its('0.contentDocument').should('exist');
+
+    return iframeSCU.its('body').should('not.be.undefined').then(cy.wrap)
+}
+//#endregion iFrame
+
 //#region Intercept
 //#endregion
 
@@ -14,10 +26,6 @@ import Common from "../common/Common"
  * @author Andrea 'Bobo' Oboe
  */
 class CampagneCommerciali {
-
-    //#region Elementi Campagne Commerciali
-    //#endregion
-
     /**
      * Verifica accesso a Campagne Commerciali
      */
@@ -35,6 +43,25 @@ class CampagneCommerciali {
 
         cy.url().should('eq', Common.getBaseUrl() + 'sales/campaign-manager')
         cy.screenshot('Verifica accesso Campagne Commerciali', { clip: { x: 0, y: 0, width: 1920, height: 900 }, overwrite: true })
+    }
+
+    static statoCampagneAttive() {
+        getIFrame().within(() => {
+            cy.intercept('POST', '**/graphql', (req) => {
+                if (req.body.operationName.includes('campaignAgent')) {
+                    req.alias = 'gqlCampaignAgent'
+                } else if (req.body.operationName.includes('campaignsMonitoringTableData')) {
+                    req.alias = 'gqlcampaignsMonitoringTableData'
+                }
+            })
+            
+            cy.contains("Verifica stato campagne attive").should('exist').and('be.visible').click()
+
+            cy.wait('@gqlCampaignAgent', { timeout: 120000 })
+            cy.wait('@gqlcampaignsMonitoringTableData', { timeout: 120000 })
+
+            cy.pause()
+        })
     }
 }
 
