c29de8e9e64b74ec76f5a98094e2be5d206cb7df
Elio Cossu <elio.cossu@allianz.it>
2022-05-19 12:13:06 +0200

    Aggiunta storno ambito

--- cypress/mw_page_objects/UltraBMP/Beneficiari.js
+++ cypress/mw_page_objects/UltraBMP/Beneficiari.js
@@ -60,19 +60,26 @@ class Beneficiari {
             cy.get('.tipo').find('.inserisci').children('button').click()
         })
 
-        cy.intercept('ultra/api/beneficiari/cerca?idAmbitoAssicurato=P_1_3&ruolo=beneficiario&tipoRischio=persona')
-            .as('anagrafe')
-        cy.wait('@anagrafe').then((interception) => {
-            cy.log(interception.request.url.body)
-            cy.openWindow(interception.request.url)
-        })
+        // cy.intercept('ultra/api/beneficiari/cerca?idAmbitoAssicurato=P_1_3&ruolo=beneficiario&tipoRischio=persona')
+        //     .as('anagrafe')
+        // cy.wait('@anagrafe').then((interception) => {
+        //     cy.log(interception.request.url.body)
+        //     cy.openWindow(interception.request.url)
+        // })
         // cy.request('ultra/api/beneficiari/cerca?idAmbitoAssicurato=P_1_3&ruolo=beneficiario&tipoRischio=persona')
         //     .then((resp) => {
         //         //cy.log(resp.url.body)
         //         cy.openWindow(resp.url)
         //     })
 
-        cy.switchWindow()
+        //cy.switchWindow()
+        cy.window().then((win) => {
+            cy.spy(win, 'open').as('windowOpen'); // 'spy' vs 'stub' lets the new tab still open if you are visually watching it
+          });
+          // perform action here [for me it was a button being clicked that eventually ended in a window.open]
+          // verify the window opened
+          // verify the first parameter is a string (this is the dynamic url) and the second is _blank (opens a new window)
+          cy.get('@windowOpen').should('be.calledWith', Cypress.sinon.match.string, '_blank');
 
         cy.get('#f-cognome')
     }
