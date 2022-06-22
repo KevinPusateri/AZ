1399ee359e02c2d1e085b0d86aa33dd2ff1cd2ed
Andrea Oboe <andrea.oboe@allianz.it>
2022-06-22 16:52:30 +0200

    Incremento wait time download

--- cypress/mw_page_objects/tenutaTariffa/TenutaTariffa.js
+++ cypress/mw_page_objects/tenutaTariffa/TenutaTariffa.js
@@ -1187,7 +1187,7 @@ class TenutaTariffa {
         cy.getIFrame()
         cy.get('@iframe').within(() => {
 
-            cy.get('motor-footer').should('exist').find('button').click().wait(5000)
+            cy.get('motor-footer').should('exist').find('button').click().wait(8000)
             cy.getTariffaLog(currentCase).then(logFolder => {
                 //#region LogTariffa
                 cy.readFile(logFolder + "\\logTariffa.xml").then(fileContent => {
