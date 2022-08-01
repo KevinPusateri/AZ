00a1c35232c0e07e7b8d2a0c0dd20d5a985f2a36
Andrea Oboe <andrea.oboe@allianz.it>
2022-08-01 16:23:42 +0200

    SafeMode Firefox 102

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -270,6 +270,9 @@ module.exports = (on, config) => {
             launchOptions.preferences['pdfjs.disabled'] = false
             launchOptions.preferences['devtools.console.stdout.content'] = false
 
+            //For Firefox 102
+            launchOptions.args.push('-safe-mode')
+
             //Necessario per queli applicativi (tipo LM) che utilizzano ancora applet java
             //Vado a prendere Allianz IO Web Ext
             launchOptions.extensions.push(process.cwd() + "\\extensions\\allianziowebext@allianz.it.xpi")
