4cffa73d6241dfb56c5e57d4ff5df5dfdbd4982a
andrea-oboe <andrea.oboe@allianz.it>
2022-08-10 14:15:06 +0200

    Edit Ricerca TFS scripts

--- scripts/tfs_ricerca.bat
+++ scripts/tfs_ricerca.bat
@@ -1,3 +1,3 @@
 set HTTP_PROXY=
 set HTTPS_PROXY=
-.\\node_modules\\.bin\\cypress run --quiet --spec cypress/integration/ricerca/*.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/ricerca, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Ricerca'"
+.\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/ricerca/*.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/ricerca, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Ricerca'"
