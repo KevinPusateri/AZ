91943bf763b9b56524046d23ba3d4eb16cbecb5f
andrea-oboe <andrea.oboe@allianz.it>
2022-08-10 10:09:26 +0200

    Edit Navigations TFS scripts

--- scripts/tfs_nav_newsInfo.bat
+++ scripts/tfs_nav_newsInfo.bat
@@ -1,3 +1,3 @@
 set HTTP_PROXY=
 set HTTPS_PROXY=
-.\\node_modules\\.bin\\cypress run --quiet --spec cypress/integration/navigation/mw_navigation_NewsInfo.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_navigation_newsInfo, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Navigation News Info'"
+.\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/navigation/mw_navigation_NewsInfo.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_navigation_newsInfo, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Navigation News Info'"
--- scripts/tfs_nav_scheda_cliente_portafoglio.bat
+++ scripts/tfs_nav_scheda_cliente_portafoglio.bat
@@ -1,3 +1,3 @@
 set HTTP_PROXY=
 set HTTPS_PROXY=
-.\\node_modules\\.bin\\cypress run --quiet --spec cypress/integration/navigation/mw_navigation_scheda_cliente_portafoglio.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_navigation_scheda_cliente_portafoglio, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Navigation Scheda Cliente Portafoglio'"
+.\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/navigation/mw_navigation_scheda_cliente_portafoglio.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_navigation_scheda_cliente_portafoglio, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Navigation Scheda Cliente Portafoglio'"
