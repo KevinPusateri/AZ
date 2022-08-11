ed4fa1f115c303c797bc48536e89148a469a3323
andrea-oboe <andrea.oboe@allianz.it>
2022-08-11 09:35:03 +0200

    Update tfs scripts for TEST env

--- scripts/tfs_nav_scheda_cliente_portafoglio.bat
+++ scripts/tfs_nav_scheda_cliente_portafoglio.bat
@@ -1,3 +1,8 @@
+@echo off
 set HTTP_PROXY=
 set HTTPS_PROXY=
-.\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/navigation/mw_navigation_scheda_cliente_portafoglio.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_navigation_scheda_cliente_portafoglio, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Navigation Scheda Cliente Portafoglio'"
+if /I %1==TEST (
+    .\\node_modules\\.bin\\cypress run --quiet --spec cypress/e2e/navigation/mw_navigation_scheda_cliente_portafoglio.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_navigation_scheda_cliente_portafoglio, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Navigation Scheda Cliente Portafoglio'"
+) else (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/navigation/mw_navigation_scheda_cliente_portafoglio.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_navigation_scheda_cliente_portafoglio, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Navigation Scheda Cliente Portafoglio'"
+)
