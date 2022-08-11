e8a00469517e5d289f9f5bfca8e3acf7e47771fe
andrea-oboe <andrea.oboe@allianz.it>
2022-08-11 09:13:18 +0200

    Update BurgerMenu tfs scripts for TEST env

--- scripts/tfs_BurgerMenu_Numbers.bat
+++ scripts/tfs_BurgerMenu_Numbers.bat
@@ -1,3 +1,8 @@
+@echo off
 set HTTP_PROXY=
 set HTTPS_PROXY=
-.\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/navigationBurger/mw_navigation_burgerMenuNumbers.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/burgerMenuNumbers, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Matrix Web Burger Menu Numbers'"
+if /I %1==TEST (
+    .\\node_modules\\.bin\\cypress run --quiet --spec cypress/e2e/navigationBurger/mw_navigation_burgerMenuNumbers.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/burgerMenuNumbers, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Matrix Web Burger Menu Numbers'"
+) else (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/navigationBurger/mw_navigation_burgerMenuNumbers.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/burgerMenuNumbers, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Matrix Web Burger Menu Numbers'"
+)
--- scripts/tfs_BurgerMenu_Sales.bat
+++ scripts/tfs_BurgerMenu_Sales.bat
@@ -1,3 +1,8 @@
+@echo off
 set HTTP_PROXY=
 set HTTPS_PROXY=
-.\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/navigationBurger/mw_navigation_burgerMenuSales.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/burgerMenuSales, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Matrix Web Burger Menu Sales'"
+if /I %1==TEST (
+    .\\node_modules\\.bin\\cypress run --quiet --spec cypress/e2e/navigationBurger/mw_navigation_burgerMenuSales.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/burgerMenuSales, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Matrix Web Burger Menu Sales'"
+) else (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/navigationBurger/mw_navigation_burgerMenuSales.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/burgerMenuSales, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Matrix Web Burger Menu Sales'"
+)
