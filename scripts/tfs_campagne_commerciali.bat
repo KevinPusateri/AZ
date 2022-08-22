93831f042ad120bdade45c2a5cd5841cd4127013
andrea-oboe <andrea.oboe@allianz.it>
2022-08-22 10:04:54 +0200

    Fix CM tfs script

--- scripts/tfs_campagne_commerciali.bat
+++ scripts/tfs_campagne_commerciali.bat
@@ -2,7 +2,7 @@
 set HTTP_PROXY=
 set HTTPS_PROXY=
 if /I %1==TEST (
-    .\\node_modules\\.bin\\cypress run --quiet --spec cypress/e2e/campagne_commerciali/*.js --env currentEnv=%1,isAviva=false --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/campagne_commerciali, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Campagne Commerciali'"
+    .\\node_modules\\.bin\\cypress run --quiet --spec cypress/e2e/campagne_commerciali/mw_campagne_commerciali.js --env currentEnv=%1,isAviva=false --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/campagne_commerciali, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Campagne Commerciali'"
 ) else (
-    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/campagne_commerciali/*.js --env currentEnv=%1,isAviva=false --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/campagne_commerciali, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Campagne Commerciali'"
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/campagne_commerciali/mw_campagne_commerciali.js --env currentEnv=%1,isAviva=false --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/campagne_commerciali, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Campagne Commerciali'"
 )
