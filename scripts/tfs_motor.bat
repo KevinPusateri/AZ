1d7212a60d2d973ce9538816ae01f8aa85ef6968
andrea-oboe <andrea.oboe@allianz.it>
2022-08-22 09:36:25 +0200

    Create TFS Script for Motor tests e2e folder

---
+++ scripts/tfs_motor.bat
@@ -0,0 +1,8 @@
+@echo off
+set HTTP_PROXY=
+set HTTPS_PROXY=
+if /I %1==TEST (
+    .\\node_modules\\.bin\\cypress run --quiet --spec cypress/e2e/motor/*.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/motor, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Motor'"
+) else (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/motor/*.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/motor, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Motor'"
+)
