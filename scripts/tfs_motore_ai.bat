44afe2f0d3019046dcf2a5677bab323b9db17441
andrea-oboe <andrea.oboe@allianz.it>
2022-08-22 09:59:05 +0200

    Update tfs motor ai script

--- scripts/tfs_motore_ai.bat
+++ scripts/tfs_motore_ai.bat
@@ -1,3 +1,8 @@
+@echo off
 set HTTP_PROXY=
 set HTTPS_PROXY=
-.\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/motor_AI/mw_controllo_fattori.js --env currentEnv=%1 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_controllo_fattori, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Motore AI'"
+if /I %1==TEST (
+    .\\node_modules\\.bin\\cypress run --quiet --spec cypress/e2e/motor_AI/mw_controllo_fattori.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_controllo_fattori, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Motore AI'"
+) else (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/motor_AI/mw_controllo_fattori.js --env currentEnv=%1 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_controllo_fattori, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Motore AI'"
+)
