b4cd5a273590b1500f5db8f22855eba114f944e1
andrea-oboe <andrea.oboe@allianz.it>
2022-08-29 09:13:30 +0200

    TFS script for Tenuta Tariffa NGRA2021 Luglio 22

--- scripts/tfs_az_rca_20220701.bat
+++ scripts/tfs_az_rca_20220701.bat
@@ -1,3 +1,8 @@
+@echo off
 set HTTP_PROXY=
 set HTTPS_PROXY=
-.\\node_modules\\.bin\\cypress run --browser E:\browsers_for_testing\GoogleChromePortable\App\Chrome-bin\chrome.exe --quiet --spec cypress/integration/motor_RCA/AZ/mw_RCA_20220701.js --env currentEnv=%1,selectedSettori=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/az_rca_20220701, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='AZ RCA Luglio 2022'"
+if /I %1==TEST (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\GoogleChromePortable\App\Chrome-bin\chrome.exe --spec cypress/e2e/motor_RCA/AZ/mw_RCA_20220701.js currentEnv=%1,selectedSettori=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/az_rca_20220701, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='AZ RCA Luglio 2022'"
+) else (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/motor_RCA/AZ/mw_RCA_20220701.js currentEnv=%1,selectedSettori=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/az_rca_20220701, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='AZ RCA Luglio 2022'"
+)
