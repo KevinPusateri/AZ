6dcc1ecaaf56f17a36c4518ac2ab2177205a9c12
Michele Delle Donne <michele.delledonne@allianz.it>
2022-09-12 15:22:04 +0200

    mw_bck_cnslt

---
+++ scripts/tfs_backoffice_consultazione.bat
@@ -0,0 +1,8 @@
+@echo off
+set HTTP_PROXY=
+set HTTPS_PROXY=
+if /I %1==TEST (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\GoogleChromePortable\App\Chrome-bin\chrome.exe --spec cypress/e2e/backoffice/consultazione/%3.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_backoffice_consultazione, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Consultazione Sinistri'"
+) else (
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/backoffice/consultazione/%3.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/mw_backoffice_consultazione, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Consultazione Sinistri'"
+)
