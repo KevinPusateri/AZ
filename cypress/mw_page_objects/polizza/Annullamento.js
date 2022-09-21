d40ac87596446dd668f9fc2e3b286cc47b2f462a
andrea-oboe <andrea.oboe@allianz.it>
2022-09-21 11:15:50 +0200

    Removed pause

--- cypress/mw_page_objects/polizza/Annullamento.js
+++ cypress/mw_page_objects/polizza/Annullamento.js
@@ -20,7 +20,6 @@ class Annullamento {
             cy.get('#dataAnnullamento').then(($firma) => {
                 const isOnlyAutografa = $firma.find('#dropTipoFirma > option')
                 if (isOnlyAutografa.length > 1){
-                    cy.pause()
                     cy.get('#dropTipoFirma').select('Autografa')
                 }
             })
