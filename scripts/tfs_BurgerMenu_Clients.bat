9f411282107311aa1d3ab4823ef3c0fe28feacac
Kevin Pusateri <kevin.pusateri@allianz.it>
2022-10-24 10:28:35 +0200

    Prova 2.0

--- scripts/tfs_BurgerMenu_Clients.bat
+++ scripts/tfs_BurgerMenu_Clients.bat
@@ -1,6 +1,8 @@
 @echo off
+set HTTP_PROXY=
+set HTTPS_PROXY=
 set HTTP_PROXY=http://it000-surf.zone2.proxy.allianz:8080
-@REM set HTTPS_PROXY=http://it000-surf.zone2.proxy.allianz:8080
+set HTTPS_PROXY=http://it000-surf.zone2.proxy.allianz:8080
 set NO_PROXY=ageallianz.it,.servizi.allianzit,.azi.allianzit
 if /I %1==TEST (
     .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\GoogleChromePortable\App\Chrome-bin\chrome.exe --spec cypress/e2e/navigationBurger/mw_navigation_burgerMenuClients.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/burgerMenuClients, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Matrix Web Burger Menu Clients'"
