205861c6da266ba11da8ad7c71d29d1fc33d4e5a
andrea-oboe <andrea.oboe@allianz.it>
2022-10-24 13:40:17 +0200

    Fix TFS RCA AVIVA missing paresentesis

--- scripts/tfs_aviva_rca.bat
+++ scripts/tfs_aviva_rca.bat
@@ -5,3 +5,4 @@ if /I %1==TEST (
     .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\GoogleChromePortable\App\Chrome-bin\chrome.exe --spec cypress/e2e/motor_RCA/AVIVA/mw_RCA_aviva.js --env currentEnv=%1 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/aviva_rca, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='RCA AVIVA'"
 ) else (
     .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/motor_RCA/AVIVA/mw_RCA_aviva.js --env currentEnv=%1 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/aviva_rca, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='RCA AVIVA'"
+)
