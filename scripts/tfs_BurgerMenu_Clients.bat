ee651b58875a32a3e96ca8bb830ffc6e25adc0d8
Kevin Pusateri <kevin.pusateri@allianz.it>
2022-10-24 10:34:37 +0200

    Prova 3 Con porta 8090

--- scripts/tfs_BurgerMenu_Clients.bat
+++ scripts/tfs_BurgerMenu_Clients.bat
@@ -1,8 +1,6 @@
 @echo off
-set HTTP_PROXY=
-set HTTPS_PROXY=
-set HTTP_PROXY=http://it000-surf.zone2.proxy.allianz:8080
-set HTTPS_PROXY=http://it000-surf.zone2.proxy.allianz:8080
+set HTTP_PROXY=http://it000-svr.zone2.proxy.allianz:8090
+set HTTPS_PROXY=http://it000-svr.zone2.proxy.allianz:8090
 set NO_PROXY=ageallianz.it,.servizi.allianzit,.azi.allianzit
 if /I %1==TEST (
     .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\GoogleChromePortable\App\Chrome-bin\chrome.exe --spec cypress/e2e/navigationBurger/mw_navigation_burgerMenuClients.js --env currentEnv=%1,isAviva=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/burgerMenuClients, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='Matrix Web Burger Menu Clients'"
