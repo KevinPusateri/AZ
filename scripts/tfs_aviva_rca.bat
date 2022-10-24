e8f813a346921d9cb0de8f980d72e7ce97202f63
andrea-oboe <andrea.oboe@allianz.it>
2022-10-24 12:37:28 +0200

    Update tfs aviva rc script

--- scripts/tfs_aviva_rca.bat
+++ scripts/tfs_aviva_rca.bat
@@ -2,6 +2,6 @@
 set HTTP_PROXY=
 set HTTPS_PROXY=
 if /I %1==TEST (
-    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\GoogleChromePortable\App\Chrome-bin\chrome.exe --spec cypress/e2e/motor_RCA/AVIVA/mw_RCA_aviva.js --env currentEnv=%1,selectedSettori=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/aviva_rca, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='AZ RCA AVIVA'"
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\GoogleChromePortable\App\Chrome-bin\chrome.exe --spec cypress/e2e/motor_RCA/AVIVA/mw_RCA_aviva.js --env currentEnv=%1 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/aviva_rca, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='AZ RCA AVIVA'"
 ) else (
-    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/motor_RCA/AVIVA/mw_RCA_aviva.js --env currentEnv=%1,selectedSettori=%2 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/aviva_rca, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='AZ RCA AVIVA'"
+    .\\node_modules\\.bin\\cypress run --quiet --browser E:\browsers_for_testing\FirefoxPortable102\App\Firefox\firefox.exe --spec cypress/e2e/motor_RCA/AVIVA/mw_RCA_aviva.js --env currentEnv=%1 --reporter cypress-mochawesome-reporter --reporter-options "reportDir=cypress/reports/aviva_rca, inlineAssets=true, charts=true, embeddedScreenshots=true, reportPageTitle='AZ RCA AVIVA'"
